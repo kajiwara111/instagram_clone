97f1c8948f21af807e43377488a65f3ed8775e8b
kajiwara111 <y.kaji.pharm1999@gmail.com>
2019-11-07 15:19:07 +0900

    #1 プロフィール編集機能新規作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    #@user.image = params[:user][:image]
+    if @user.update(user_params)
+      flash[:notice] = 'プロフィール画像を更新しました'
+      redirect_to user_path(@user.id)
+    else
+      flash[:danger] = 'プロフィール画像の更新に失敗しました'
+      render :edit
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_chache)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, unless: :password_digest
   has_secure_password
   has_many :feeds
   has_many :favorites, dependent: :destroy
+  mount_uploader :image, ImageUploader
 end
